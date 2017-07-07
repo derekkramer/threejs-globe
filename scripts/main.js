ee8c7673165ccacd08e857955db82e9dd461c68e
Derek Kramer <derek.kramer88@gmail.com>
2017-07-07 14:45:54 -0600

    🌎 Working static globe

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
-var mesh;
+let mesh;
+let rotating = 0.005;
 
 const renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -16,7 +17,7 @@ textureLoader.load('https://c1.staticflickr.com/3/2521/3884071286_edb50f8137_b.j
     texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
     texture.repeat.set(1, 1);
 
-    const material = new THREE.MeshLambertMaterial({map: texture});
+    const material = new THREE.MeshPhongMaterial({map: texture});
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
@@ -28,8 +29,45 @@ camera.position.z = 200;
 const light = new THREE.AmbientLight(0x404040, 4);
 scene.add(light);
 
+let isDragging = false;
+let previousPos = {
+    x: 0,
+    y: 0
+};
+
+$(renderer.domElement).on('mousedown', () => {
+    isDragging = true;
+    rotating = 0;
+    previousPos = {
+        x: 0,
+        y: 0
+    };
+})
+.on('mousemove', (event) => {
+    if(isDragging){
+        if(!previousPos.x && !previousPos.y){
+            previousPos.x = event.pageX;
+            previousPos.y = event.pageY;
+        }else{
+            let changeX = event.pageX - previousPos.x,
+                changeY = event.pageY - previousPos.y;
+
+            previousPos.x = event.pageX;
+            previousPos.y = event.pageY;
+
+            mesh.rotation.x += changeY * 0.005;
+            mesh.rotation.y += changeX * 0.005;
+        }
+    }
+});
+
+$(document).on('mouseup', () => {
+    isDragging = false;
+    rotating = 0.005;
+});
+
 var render = function() {
     requestAnimationFrame(render);
-    mesh.rotation.y += 0.005;
+    mesh.rotation.y += rotating;
     renderer.render(scene, camera);
 };
