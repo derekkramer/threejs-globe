0a349b87efccc4a127a9b5fe164de753dcfb543e
Derek Kramer <derek.kramer88@gmail.com>
2017-07-07 18:07:49 -0600

    ✨ Added server, bump mapping, specular mapping, and star field

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,40 +1,78 @@
+// Initialize the scene, camera, and global variables
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
-let mesh;
-let rotating = 0.005;
+let earthMesh,
+    starMesh,
+    rotating = 0.005;
 
+// Initialize the renderer and append to the HTML
 const renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.SphereGeometry(24, 32, 32);
-// THREE.ImageUtils.crossOrigin = true;
+// Create a new sphere
+const earthGeo = new THREE.SphereGeometry(24, 32, 32);
+const starGeo = new THREE.SphereGeometry(500, 32, 32);
 
-const textureLoader = new THREE.TextureLoader();
-textureLoader.crossOrigin = true;
+// Initialize the texture loader
+const loader = new THREE.TextureLoader();
 
-textureLoader.load('https://c1.staticflickr.com/3/2521/3884071286_edb50f8137_b.jpg', texture => {
-    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(1, 1);
+// Load the textures
+const earthMap = loader.load('./img/earthmap1k.jpg');
+const earthBump = loader.load('./img/earthbump1k.jpg');
+const earthSpec = loader.load('./img/earthspec1k.jpg');
+const starfield = loader.load('./img/starfield.jpg');
 
-    const material = new THREE.MeshPhongMaterial({map: texture});
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+const earthMat = new THREE.MeshPhongMaterial(
+    {
+        map: earthMap,
+        bumpMap: earthBump,
+        bumpScale: 0.5,
+        specularMap: earthSpec,
+        specular: 0x222222
+    }
+);
 
-    render();
-});
+const starMat = new THREE.MeshBasicMaterial(
+    {
+        map: starfield,
+        side: THREE.BackSide
+    }
+);
 
+starMesh = new THREE.Mesh(starGeo, starMat);
+// scene.add(starMesh);
+
+earthMesh = new THREE.Mesh(earthGeo, earthMat);
+earthMesh.add(starMesh);
+scene.add(earthMesh);
+
+// Position the camera back
 camera.position.z = 200;
 
-const light = new THREE.AmbientLight(0x404040, 4);
-scene.add(light);
+// Create and add lights to the scene
+const ambientLight = new THREE.AmbientLight(0x404040, 2);
+scene.add(ambientLight);
 
+const directionalLight = new THREE.DirectionalLight(0x404040, 4);
+directionalLight.position.set(3, 3, 3);
+directionalLight.target.position.set(0, 0, 0);
+// scene.add(directionalLight);
+earthMesh.add(directionalLight);
+
+scene.add(earthMesh);
+
+// Start the rendering loop
+render();
+
+// Initialize interactive variables
 let isDragging = false;
 let previousPos = {
     x: 0,
     y: 0
 };
 
+// If the mouse is clicked, alter the rotation of the earth
 $(renderer.domElement).on('mousedown', () => {
     isDragging = true;
     rotating = 0;
@@ -46,28 +84,34 @@ $(renderer.domElement).on('mousedown', () => {
 .on('mousemove', (event) => {
     if(isDragging){
         if(!previousPos.x && !previousPos.y){
-            previousPos.x = event.pageX;
-            previousPos.y = event.pageY;
+            setPrevious();
         }else{
             let changeX = event.pageX - previousPos.x,
                 changeY = event.pageY - previousPos.y;
 
-            previousPos.x = event.pageX;
-            previousPos.y = event.pageY;
+            setPrevious();
 
-            mesh.rotation.x += changeY * 0.005;
-            mesh.rotation.y += changeX * 0.005;
+            earthMesh.rotation.x += changeY * 0.005;
+            earthMesh.rotation.y += changeX * 0.005;
         }
     }
 });
 
+// If released, reset the interactive variables
 $(document).on('mouseup', () => {
     isDragging = false;
     rotating = 0.005;
 });
 
-var render = function() {
+// The render loop
+function render(){
     requestAnimationFrame(render);
-    mesh.rotation.y += rotating;
+    earthMesh.rotation.y += rotating;
     renderer.render(scene, camera);
-};
+}
+
+// Setting the previous mouse coordinates to the current coordiantes
+function setPrevious(){
+    previousPos.x = event.pageX;
+    previousPos.y = event.pageY;
+}
